8dfb656896b2705017b3a954afa564d6c55cd007
Tiago Calogero <t.calogero@digialta.com.br>
2017-03-01 18:30:21 -0300

    Added vue-material

--- ecommerce--webschool/src/App.vue
+++ ecommerce--webschool/src/App.vue
@@ -1,16 +1,39 @@
 <template>
-  <cc-hello></cc-hello>
+	<header class="phone-viewport">
+		<md-toolbar>
+			<md-button class="md-icon button" @click.native="toggleLeftSidenav">
+				<md-icon>menu</md-icon>
+			</md-button>
+			
+			<h1 class="md-title">{{title}}</h1>
+		</md-toolbar>
+		
+		<md-sidenav class="md-left" ref="leftSidenav">
+			<md-toolbar class="md-medium">
+				<div class="md-toolbar-container">
+					<h2 class="md-title">Sidenav</h2>
+				</div>
+			</md-toolbar>
+			
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, ullam voluptate! Ab accusamus aperiam blanditiis consequuntur, dicta facere fugiat, maiores, necessitatibus nostrum nulla odit quos sint tempore vero voluptatum! Eum!</p>
+		</md-sidenav>
+	</header>
 </template>
 
 <script>
-import CcHello from './components/Hello'
-
-export default {
-  name: 'TestComponent',
-  components: {
-    CcHello
-  }
-}
+	export default {
+		name: 'TestComponent',
+		data (){
+			return {
+				title: "Test title"
+			}
+		},
+		methods: {
+			toggleLeftSidenav(){
+				this.$refs.leftSidenav.toggle()
+			}
+		}
+	}
 </script>
 
 <style>
--- ecommerce--webschool/src/main.js
+++ ecommerce--webschool/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+Vue.use(VueMaterial)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
